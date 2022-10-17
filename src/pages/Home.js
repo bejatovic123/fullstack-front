3a635497779c1d7140207025221cfbb982693b18
Bratislav Bejatovic <bratislavbeat@gmail.com>
2022-10-18 01:36:32 +0200

    AXios

---
+++ src/pages/Home.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get('http://localhost:8080/users');
+    console.log(result.data);
+  };
+
+  return (
+    <div className='container'>
+      <div classname='py-4'>
+        <table className='table border shadow'>
+          <thead>
+            <tr>
+              <th scope='col'>#</th>
+              <th scope='col'>First</th>
+              <th scope='col'>Last</th>
+              <th scope='col'>Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope='row'>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
