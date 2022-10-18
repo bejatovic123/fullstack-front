8f995c3707a82cbf08cedeb21d401ae0f912e7e8
Bratislav Bejatovic <bratislavbeat@gmail.com>
2022-10-18 02:53:30 +0200

    post data axios

--- src/users/AddUsers.js
+++ src/users/AddUsers.js
@@ -1,6 +1,10 @@
+import axios from 'axios';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function AddUsers() {
+  let navigate = useNavigate();
+
   const [user, setUser] = useState({
     name: '',
     username: '',
@@ -13,57 +17,65 @@ export default function AddUsers() {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post('http://localhost:8080/user', user);
+    navigate('/');
+  };
+
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center mb-4'>Register User</h2>
-          <div className='mb-3'>
-            <label htmlFor='Name' className='form-label'>
-              Name
-            </label>
-            <input
-              type={'text'}
-              className='form-control'
-              placeholder='Enter your name'
-              name='name'
-              value={name}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <div className='mb-3'>
-            <label htmlFor='Username' className='form-label'>
-              Username
-            </label>
-            <input
-              type={'text'}
-              className='form-control'
-              placeholder='Enter your name'
-              name='username'
-              value={username}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <div className='mb-3'>
-            <label htmlFor='Email' className='form-label'>
-              Email
-            </label>
-            <input
-              type={'text'}
-              className='form-control'
-              placeholder='Enter your name'
-              name='email'
-              value={email}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <button type='submit' className='btn btn-outline-primary'>
-            Submit
-          </button>
+          <form onSubmit={(e) => onSubmit(e)}>
+            <div className='mb-3'>
+              <label htmlFor='Name' className='form-label'>
+                Name
+              </label>
+              <input
+                type={'text'}
+                className='form-control'
+                placeholder='Enter your name'
+                name='name'
+                value={name}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='Username' className='form-label'>
+                Username
+              </label>
+              <input
+                type={'text'}
+                className='form-control'
+                placeholder='Enter your name'
+                name='username'
+                value={username}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='Email' className='form-label'>
+                Email
+              </label>
+              <input
+                type={'text'}
+                className='form-control'
+                placeholder='Enter your name'
+                name='email'
+                value={email}
+                onChange={(e) => onInputChange(e)}
+              />
+            </div>
+            <button type='submit' className='btn btn-outline-primary'>
+              Submit
+            </button>
 
-          <button type='submit' className='btn btn-outline-danger mx-2'>
-            Cancel
-          </button>
+            <button type='submit' className='btn btn-outline-danger mx-2'>
+              Cancel
+            </button>
+          </form>
         </div>
       </div>
     </div>
